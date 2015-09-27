ba8d8c47f82f51f2a97ad2d97e04d1abdc21b2a0
Akiva Leffert <aleffert@gmail.com>
2015-09-27 10:54:59 -0400

    Register constraint info

--- iOS/DLSViewsPlugin+SnapKitSupport.swift
+++ iOS/DLSViewsPlugin+SnapKitSupport.swift
@@ -8,9 +8,61 @@
 
 import Foundation
 import Dials
+import SnapKit
+
+private let identifier = "com.akivaleffert.snaps"
+
+class ConstraintInfo : NSObject, NSCoding {
+    let file : String
+    let line : Int
+    
+    init?(constraint : NSLayoutConstraint) {
+        if let location = (constraint as? LayoutConstraint)?.snp_constraint?.location {
+            self.file = location.file
+            self.line = Int(location.line)
+            super.init()
+        }
+        else {
+            self.file = ""
+            self.line = -1
+            super.init()
+            return nil
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        if let file = coder.decodeObjectForKey("file") as? String {
+            self.file = file
+            line = coder.decodeIntegerForKey("line")
+            super.init()
+        }
+        else {
+            self.file = ""
+            self.line = -1
+            super.init()
+            return nil
+        }
+    }
+    
+    func encodeWithCoder(coder : NSCoder) {
+        coder.encodeObject(file, forKey: "file")
+        coder.encodeInteger(line, forKey: "line")
+    }
+}
+
+class ConstraintInformer : NSObject, DLSConstraintInformer {
+    func infoForConstraint(constraint: NSLayoutConstraint) -> DLSAuxiliaryConstraintInformation? {
+        if let info = ConstraintInfo(constraint: constraint) {
+            return DLSAuxiliaryConstraintInformation(pluginIdentifier: identifier, userData: info)
+        }
+        else {
+            return nil
+        }
+    }
+}
 
 extension DLSViewsPlugin {
     public func enableSnapKitSupport() {
-        
+        DLSViewsPlugin.activePlugin()?.addAuxiliaryConstraintInformer(ConstraintInformer())
     }
 }
